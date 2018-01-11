931b2c9aeffea18eaf2739837a51470370011275
Tordenliden <fls@tec.dk>
2018-01-11 13:15:57 +0100

    Skulle lige experimentere med noget Generics

---
+++ Vanish/Datalag/GenericClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Vanish.Modeller;
+
+namespace Vanish.Datalag
+{
+    class GenericClass
+    {
+
+        public void aa()
+        {
+
+            //Kunde k = new Kunde();
+            //Type t = k.GetType();
+            ////PropertyInfo[] props = t.GetProperties()[
+            //k.GetType().GetProperties()[1].SetValue(k, "bo");
+
+            int tal = 99;
+        }
+    }
+}
